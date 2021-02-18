910f62622af8b46a41dec24380232208cb2a0774
Matheus Silva <teus_26@outlook.com>
2021-02-18 15:35:15 -0300

    Add files via upload

---
+++ eclipse-workspace/sintaxe-variaveis-e-fluxo/src/PontoFlutuante.java
@@ -0,0 +1,10 @@
+
+public class PontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double salario = 7250.70;
+		
+		System.out.println("Meu salário é " + salario);
+	}
+}
---
+++ eclipse-workspace/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,14 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Olá, novo teste");
+		
+		int idade;
+		idade = 21;
+		
+		System.out.println(idade);
+		
+		System.out.println("Minha idade é " + idade);
+	}
+}
